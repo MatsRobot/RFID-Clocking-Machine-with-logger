6744af61d0d33656b5a56de60039cc87d7fef062
Mat <121375703+MatsRobot@users.noreply.github.com>
2024-08-28 09:41:39 +0100

    Add files via upload

---
+++ CopyFileToDesktop.bat
@@ -0,0 +1,15 @@
+set local
+@echo off
+cls
+
+for /f "tokens=1-5*" %%1 in ('vol') do (
+    set volume=%%6 & goto echoit
+)
+:echoit
+
+set year=%date:~6,4%
+set month=%date:~3,2%
+set day=%date:~0,2%
+set timestamp="%volume%- %year%%month%%day%"
+
+copy ESIGN.CSV C:\Users\HP\Desktop\eSign-%timestamp%.CSV
